1d8126f01ac39ac481a20006ea9285a45205d40a
vitoriasaturnino <vim.saturnino@gmail.com>
2023-09-21 11:30:17 -0300

    build: add plug_cowboy as a dependency

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule ApiRestPlugCowboy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :plug_cowboy],
       mod: {ApiRestPlugCowboy.Application, []}
     ]
   end
@@ -33,7 +33,9 @@ defmodule ApiRestPlugCowboy.MixProject do
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, "~> 0.21.3", only: :dev, runtime: false},
       {:inch_ex, github: "rrrene/inch_ex", only: [:dev, :test]},
-      {:excoveralls, "~> 0.12.3", only: :test}
+      {:excoveralls, "~> 0.12.3", only: :test},
+      {:plug_cowboy, "~> 2.2"}, # server
+      {:poison, "~> 4.0.1"} # json encode/decode
     ]
   end
 
